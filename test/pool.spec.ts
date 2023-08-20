fe0a0dd4e592b3b3ff6239e429121e9cd5453439
Samuele Marro <marrosamuele@gmail.com>
2023-08-20 19:50:08 +0200

    Added more sanity unit tests.

--- test/pool.spec.ts
+++ test/pool.spec.ts
@@ -270,9 +270,8 @@ describe('test BasePool', function () {
         })
 
         describe('liquidity', function () {
-            it.only('deposits liquidity', async function () {
+            it('deposits liquidity', async function () {
                 await contract.initialize(encodePriceSqrt(BigNumber.from(1)))
-                console.log(await contract.slot0())
 
                 const mintParams = {
                     token0 : TOKEN_0,
@@ -281,7 +280,7 @@ describe('test BasePool', function () {
                     tickLower : -887272,
                     tickUpper : 887272,
                     amount0Desired : 1000,
-                    amount1Desired : 1000,
+                    amount1Desired : 2000,
                     amount0Min : 0,
                     amount1Min : 0,
                     recipient : deployer.address,
@@ -289,15 +288,98 @@ describe('test BasePool', function () {
                 }
 
                 await token0Contract.connect(deployer).approve(positionManagerContract.address, '1000')
-                await token1Contract.connect(deployer).approve(positionManagerContract.address, '1000')
-                hre.tracer.enabled = true
-                const tx = await positionManagerContract.connect(deployer).mint(mintParams)
-                console.log(await tx.wait())
-                hre.tracer.enabled = false
+                await token1Contract.connect(deployer).approve(positionManagerContract.address, '2000')
+                await positionManagerContract.connect(deployer).mint(mintParams)
 
             })
-            it('withdraws liquidity', async function () {
-                // TODO
+            it('deposits and increases liquidity', async function () {
+                await contract.initialize(encodePriceSqrt(BigNumber.from(1)))
+
+                const mintParams = {
+                    token0 : TOKEN_0,
+                    token1 : TOKEN_1,
+                    fee : 5, // TODO: Figure it out
+                    tickLower : -887272,
+                    tickUpper : 887272,
+                    amount0Desired : 1000,
+                    amount1Desired : 2000,
+                    amount0Min : 0,
+                    amount1Min : 0,
+                    recipient : deployer.address,
+                    deadline : await time.latest() + 1000000
+                }
+
+                await token0Contract.connect(deployer).approve(positionManagerContract.address, '1000')
+                await token1Contract.connect(deployer).approve(positionManagerContract.address, '2000')
+                await positionManagerContract.connect(deployer).mint(mintParams)
+
+                const increaseParams = {
+                    tokenId : 1,
+                    amount0Desired : 50,
+                    amount1Desired : 100,
+                    amount0Min: 0,
+                    amount1Min : 0,
+                    deadline: await time.latest() + 1000000
+                }
+
+                await token0Contract.connect(deployer).approve(positionManagerContract.address, '50')
+                await token1Contract.connect(deployer).approve(positionManagerContract.address, '100')
+
+                await positionManagerContract.connect(deployer).increaseLiquidity(increaseParams)
+
+            })
+            it('deposits and decreases liquidity', async function () {
+                await contract.initialize(encodePriceSqrt(BigNumber.from(2)))
+
+                const mintParams = {
+                    token0 : TOKEN_0,
+                    token1 : TOKEN_1,
+                    fee : 5, // TODO: Figure it out
+                    tickLower : -887272,
+                    tickUpper : 887272,
+                    amount0Desired : 1000,
+                    amount1Desired : 3000,
+                    amount0Min : 0,
+                    amount1Min : 0,
+                    recipient : deployer.address,
+                    deadline : await time.latest() + 1000000
+                }
+
+                const initialToken0Balance = await token0Contract.balanceOf(deployer.address)
+                const initialToken1Balance = await token1Contract.balanceOf(deployer.address)
+
+                await token0Contract.connect(deployer).approve(positionManagerContract.address, '1000')
+                await token1Contract.connect(deployer).approve(positionManagerContract.address, '3000')
+                await positionManagerContract.connect(deployer).mint(mintParams)
+
+                await checkQuery('liquidity', [], [1414], contract)
+
+                const decreaseParams = {
+                    tokenId : 1,
+                    liquidity : 707, // Corresponding to 50% of the current liqudity
+                    amount0Min : 0,
+                    amount1Min : 0,
+                    deadline : await time.latest() + 1000000
+                }
+
+                await positionManagerContract.connect(deployer).decreaseLiquidity(decreaseParams)
+
+                await checkQuery('liquidity', [], [707], contract)
+                // Since the current ratio was 2:1, the obtained amounts are 2:1 as well
+                // Note that the amounts are not exact because of rounding
+                await checkQuery('tokensOwed', [1], [499, 999], positionManagerContract)
+
+                const collectParams = {
+                    tokenId : 1,
+                    recipient : deployer.address,
+                    amount0Max : 1000,
+                    amount1Max : 2000
+                }
+
+                await positionDescriptorContract.connect(deployer).collect(collectParams)
+
+                await checkQuery('balanceOf', [deployer.address], [initialToken0Balance.sub(1000).add(100)], token0Contract)
+                await checkQuery('balanceOf', [deployer.address], [initialToken1Balance.sub(2000).add(200)], token1Contract)
             })
             it('fails to deposit with a past lock deadline', async function () {
                 // TODO
@@ -311,6 +393,25 @@ describe('test BasePool', function () {
             it('swaps', async function () {
                 // TODO
                 // TODO: Check that the revenue is split correctly
+            })
+        })
+
+        describe('setting protocol fees', function () {
+            it('sets the protocol fee', async function () {
+                // TODO
+                // The fee breakdown is:
+                // Total fee: 0.25%
+                //   0.20% goes to LPs
+                //   0.05% (aka 1/5th of the fee) goes to the protocol
+                //     0.01% goes to the treasury
+                //     0.02% goes to VINU Buy & Burn
+                //     0.02% goes to VC Buy & Burn
+            })
+            it('fails to set the protocol fee if not the owner', async function () {
+                // TODO
+            })
+            it('fails to set the protocol fee if the fee is too high', async function () {
+                // TODO
             })
         })
 
