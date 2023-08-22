6923bdaa40390a9f3ba046ab15218bb91f7d69b9
Samuele Marro <marrosamuele@gmail.com>
2023-08-22 17:55:35 +0200

    Fixed IVinuSwapPoolDeployer docs.

--- contracts/core/interfaces/IVinuSwapPoolDeployer.sol
+++ contracts/core/interfaces/IVinuSwapPoolDeployer.sol
@@ -8,11 +8,11 @@ pragma solidity >=0.5.0;
 interface IVinuSwapPoolDeployer {
     /// @notice Get the parameters to be used in constructing the pool, set transiently during pool creation.
     /// @dev Called by the pool constructor to fetch the parameters of the pool
-    /// Returns factory The factory address
-    /// Returns token0 The first token of the pool by address sort order
-    /// Returns token1 The second token of the pool by address sort order
-    /// Returns fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
-    /// Returns tickSpacing The minimum number of ticks between initialized ticks
+    /// @return factory The factory address
+    /// @return token0 The first token of the pool by address sort order
+    /// @return token1 The second token of the pool by address sort order
+    /// @return fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
+    /// @return tickSpacing The minimum number of ticks between initialized ticks
     function parameters()
         external
         view
