0560ae0696be9e36094323236384328125f5cbe9
Samuele Marro <marrosamuele@gmail.com>
2023-08-22 10:45:42 +0200

    Added IFeeManager.sol.

---
+++ contracts/core/interfaces/IFeeManager.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+pragma abicoder v2;
+
+/// @title Fee Manager
+/// @notice Allows computing fees dinamically
+interface IFeeManager {
+    /// @notice Computes the actual fee pips
+    /// @param fee The current fee pips
+    function computeFee(uint24 fee) external returns (uint24);
+}
